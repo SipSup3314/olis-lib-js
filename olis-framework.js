dc0e1af7ad59fe3a62a12ff5903f59b58a84379b
Oli S <69776090+SipSup3314@users.noreply.github.com>
2022-04-26 09:49:49 -0400

    added randDec function

--- olis-framework.js
+++ olis-framework.js
@@ -1,3 +1,6 @@
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+function randDec(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)+Math.random();
+}
