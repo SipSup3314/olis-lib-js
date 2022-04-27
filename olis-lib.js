83d469c5a97b7b82375a91ee2548df5e0ac09efa
Oli S <69776090+SipSup3314@users.noreply.github.com>
2022-04-27 13:48:14 -0400

    Delete olis-lib.js

--- olis-lib.js
+++
@@ -1,6 +0,0 @@
-function randInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-function randDec(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)+Math.random();
-}
