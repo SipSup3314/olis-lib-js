00f92ff87f4f25b82a3858c78de3800fd1046ed5
Oli S <69776090+SipSup3314@users.noreply.github.com>
2022-04-27 13:47:08 -0400

    Create olis-lib-1-0.js

---
+++ archive/olis-lib-1-0.js
@@ -0,0 +1,6 @@
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function randDec(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)+Math.random();
+}
