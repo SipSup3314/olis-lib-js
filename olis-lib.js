622808680dbce2db1a3448060afecd25c8322daf
Oli S <69776090+SipSup3314@users.noreply.github.com>
2022-04-30 23:11:29 -0400

    added chance function

--- olis-lib.js
+++ olis-lib.js
@@ -4,3 +4,8 @@ function randInt(min, max) {
 function randDec(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)+Math.random();
 }
+function chance(percentage){
+    if (Math.floor(Math.random() * 101) >= 100-percentage) {
+        return true;
+    }
+}
