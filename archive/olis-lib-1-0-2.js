08bd24694c55a53d993ab26ca677fca8e2059cff
Oli S <69776090+SipSup3314@users.noreply.github.com>
2022-05-18 14:37:20 -0400

    Create olis-lib-1-0-2.js

---
+++ archive/olis-lib-1-0-2.js
@@ -0,0 +1,17 @@
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function randDec(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)+Math.random();
+}
+function chance(percentage){
+    if (Math.floor(Math.random() * 101) >= 100-percentage) {
+        return true;
+    }
+}
+function avg(array) {
+    return array.reduce((a, b) => a + b) / array.length;
+}
+function round(num1,num2){
+    return num2*(Math.round(num1/num2));
+}
